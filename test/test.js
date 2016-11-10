5135e2146becf2c75d9db38e3921aaf7bcb1ca8a
Jingshao Chen <jingshaochen@gmail.com>
2016-11-09 23:33:41 -0500

    init

---
+++ test/test.js
@@ -0,0 +1,17 @@
+const Light = require('./light.js')
+
+var light1 = new Light(2, [1, 1])
+var light2 = new Light(4, [2, 2])
+
+light1.start()
+light2.start()
+
+setTimeout(
+  function () {
+    light1.stop()
+    light2.stop()
+    light1.destroy()
+    light2.destroy()
+  }, 5000)
+
+
